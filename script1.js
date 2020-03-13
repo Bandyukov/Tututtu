3aebae4113c84bb2e981b6dc3b58668e9f663a1f
Aleksei <yolo@gmail.com>
2020-03-13 11:55:17 +0300

    first commit

---
+++ script1.js
@@ -0,0 +1,31 @@
+
+var leftBorderWidth = 1;
+let second = 2, flag = false;
+const pi = 3.14;
+
+console.log(second / 0);
+console.log('tutu' - 8);
+
+let person = { name: 'John', age: 24, married: flag};
+console.log(person.name, person.age, person.married);
+
+let arr = ['apple', 'pamello'];
+console.log(arr[5]);
+
+//alert("What have u forgotten gere?");
+//let answer = confirm("Are you ok, dude?");
+//console.log(answer);
+let respond = prompt("Есть ли вам 18 eptel'?", "Да");
+console.log("User's respond:", respond);
+let a = 0.9, b = 19, flag1 = true, flag2 = false;
+
+for(var i = 0; i < 6; i++)
+{
+    a = a * i;
+    b -= i;
+}
+
+console.log(a, b);
+console.log(!flag1 || flag2);
+
+
