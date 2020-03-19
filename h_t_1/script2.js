fa8d4a9ffc5ba0c5bff0c3c128fd303a57524db5
Aleksei <yolo@gmail.com>
2020-03-19 18:58:14 +0300

    k_1

--- h_t_1/script2.js
+++ h_t_1/script2.js
@@ -1,12 +1,10 @@
 var money, time;
 
-function strart()
-{
+function strart() {
     money = +prompt("Ваш бюджет на месяц?");
     time = prompt("Введите дату в формате YYYY-MM-DD");
 
-    while (isNaN(money) || money == "" || money == null)
-    {
+    while (isNaN(money) || money == "" || money == null) {
         money = +prompt("Ваш бюджет на месяц?");
     }
 }
